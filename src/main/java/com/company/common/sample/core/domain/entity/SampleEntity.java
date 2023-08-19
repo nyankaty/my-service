8bf5455240b16ffa1bb036540ca10884a0bffd9a
VinhTQ <VinhTQ@vissoft.vn>
2023-08-19 17:34:39 +0700

    .

--- src/main/java/com/company/common/sample/core/domain/entity/SampleEntity.java
+++ src/main/java/com/company/common/sample/core/domain/entity/SampleEntity.java
@@ -3,13 +3,23 @@ package com.company.common.sample.core.domain.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "sample")
 public class SampleEntity {
 
+    @Id
     private Long id;
 
+    @Column(name = "column_one")
     private String columnOne;
 
+    @Column(name = "column_two")
     private String columnTwo;
 }
