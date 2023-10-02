9f21ccb873795120a295e2ec2e3add59aa53fb0b
VinhTQ <VinhTQ@vissoft.vn>
2023-10-03 06:23:33 +0700

    .

--- src/main/java/com/company/common/sample/ui/restful/SampleOperations.java
+++ src/main/java/com/company/common/sample/ui/restful/SampleOperations.java
@@ -2,13 +2,12 @@ package com.company.common.sample.ui.restful;
 
 import com.company.common.sample.core.domain.dto.request.SampleReqDto;
 import com.company.common.sample.core.domain.dto.response.SampleResDto;
-import com.company.common.spring.factory.request.PagingRequest;
 import com.company.common.spring.factory.response.Paging;
 import com.company.common.spring.factory.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 
@@ -19,4 +18,7 @@ public interface SampleOperations {
     @Operation(summary = "Get example")
     @GetMapping( "/example")
     Response<Paging<SampleResDto>> example(@Valid SampleReqDto request);
+
+    @GetMapping("/redirect")
+    String get();
 }
--- src/main/java/com/company/common/sample/ui/restful/impl/SampleController.java
+++ src/main/java/com/company/common/sample/ui/restful/impl/SampleController.java
@@ -3,18 +3,15 @@ package com.company.common.sample.ui.restful.impl;
 import com.company.common.sample.core.domain.dto.request.SampleReqDto;
 import com.company.common.sample.core.domain.dto.response.SampleResDto;
 import com.company.common.sample.core.service.SampleService;
+import com.company.common.sample.infrastructure.client.ClientTemplateAdapter;
 import com.company.common.sample.ui.restful.SampleOperations;
-import com.company.common.spring.factory.request.PagingRequest;
 import com.company.common.spring.factory.response.Paging;
 import com.company.common.spring.factory.response.Response;
-import com.company.common.spring.factory.response.ResponseFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 
 @RestController
@@ -23,9 +20,18 @@ import javax.validation.Valid;
 public class SampleController implements SampleOperations {
 
     private final SampleService sampleService;
+    private final ClientTemplateAdapter clientTemplate;
 
     @Override
     public Response<Paging<SampleResDto>> example(SampleReqDto request) {
         return Response.of(sampleService.findAll(request));
+    }
+
+    @Override
+    public String get() {
+//        return Mono.just("sgd");
+        return WebClient.create("http://192.168.0.103:7777").get().uri("/config")
+                .retrieve().bodyToMono(String.class).block();
+//        return clientTemplate.get("http://localhost:7777/config", null, 10, String.class);
     }
 }
