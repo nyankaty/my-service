77ef643fe359cbdf883bd60605a1a5dc2c036d8e
VinhTQ <VinhTQ@vissoft.vn>
2023-10-11 08:27:16 +0700

    .

--- src/main/java/com/company/common/sample/infrastructure/externalcache/ExternalCacheTemplateAdapter.java
+++ src/main/java/com/company/common/sample/infrastructure/externalcache/ExternalCacheTemplateAdapter.java
@@ -2,13 +2,9 @@ package com.company.common.sample.infrastructure.externalcache;
 
 import com.company.common.cache.external.implementation.RedisCacheTemplate;
 import com.company.common.cache.external.port.ExternalCacheTemplate;
-import com.company.common.cache.external.properties.RedisCacheConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExternalCacheTemplateAdapter extends RedisCacheTemplate implements ExternalCacheTemplate {
 
-    public ExternalCacheTemplateAdapter(RedisCacheConfigurationProperties externalCacheProp) {
-        super(externalCacheProp);
-    }
 }
